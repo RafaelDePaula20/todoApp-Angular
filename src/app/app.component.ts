e8b1d6b8f52f9b60b45fd9b4bbe65faa1dbafd29
Rafael13 <rafaeldepaula1325@gmail.com>
2023-11-01 20:26:52 +0000

    Feat: finish work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Todo } from 'src/models/todo.model';
 })
 export class AppComponent {
 
+  public mode = 'list';
   public todos: Todo[] = [];
   public title: string ='Minhas Tarefas';
   public form: FormGroup;
@@ -23,6 +24,8 @@ export class AppComponent {
         Validators.required,
       ])]
     });
+
+    this.load();
   }
   
   add() {
@@ -30,6 +33,7 @@ export class AppComponent {
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
     this.todos.push(new Todo(id, title, false));
+    this.save();
     this.clear();
   }
 
@@ -42,13 +46,37 @@ export class AppComponent {
     if(index !== -1){
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
   }
+
+  save() { 
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+    this.mode = 'list';
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if(data) {
+      this.todos = JSON.parse(data);
+    } else { 
+      this.todos = [];
+    }
+
+  }
+
+  changeMode(mode: string) {
+    this.mode = mode;
+  }
+
 }
