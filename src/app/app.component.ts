9f5d9ff4b0e7b422b5c7b23a8a05369fe818f64f
Rafael13 <rafaeldepaula1325@gmail.com>
2023-11-01 19:05:29 +0000

    Feat: add task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,12 +23,20 @@ export class AppComponent {
         Validators.required,
       ])]
     });
-
-    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
-    this.todos.push(new Todo(2, 'Ir ao supermercado', false));
-    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
   }
   
+  add() {
+    //this.form.value =>{title: 'Titulo'}
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
+  }
+
   remove(todo: Todo){
     const index = this.todos.indexOf(todo);
     if(index !== -1){
